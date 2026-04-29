b598450cf75ff96b84d5e373b8cc46cc268bfbca
shaharshLap <shaharshpabbathi@gmail.com>
2026-04-29 13:45:45 +0530

    engg team added

--- stock_picker/src/stock_picker/crew.py
+++ stock_picker/src/stock_picker/crew.py
@@ -2,13 +2,13 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-from crewai_tools import SerperDevTool
-from crewai.memory import LongTermMemory, ShortTermMemory, EntityMemory
-from crewai.storage import RAGStorage, LTMSQLiteStorage
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
-class FinancialresearcherAi():
-    """FinancialresearcherAi crew"""
+class StockPicker():
+    """StockPicker crew"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
@@ -23,14 +23,13 @@ class FinancialresearcherAi():
     def researcher(self) -> Agent:
         return Agent(
             config=self.agents_config['researcher'], # type: ignore[index]
-            tools=[SerperDevTool()],
             verbose=True
         )
 
     @agent
-    def analyst(self) -> Agent:
+    def reporting_analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config['analyst'], # type: ignore[index]
+            config=self.agents_config['reporting_analyst'], # type: ignore[index]
             verbose=True
         )
 
@@ -44,53 +43,22 @@ class FinancialresearcherAi():
         )
 
     @task
-    def analysis_task(self) -> Task:
+    def reporting_task(self) -> Task:
         return Task(
-            config=self.tasks_config['analysis_task'], # type: ignore[index]
-            output_file='/output/{company}/report.md'
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md'
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the FinancialresearcherAi crew"""
+        """Creates the StockPicker crew"""
         # To learn how to add knowledge sources to your crew, check out the documentation:
         # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-        short_term_memory = ShortTermMemory(
-            storage=RAGStorage(
-                embedder_config={
-                    "provider": "openai",
-                    "config": {
-                        "model": "text-embedding-3-small",
-                    }
-                }, 
-                type="short_term",
-                path="./memory/"
-            )
-        )
-        long_term_memory = LongTermMemory(
-            storage=LTMSQLiteStorage(
-                db_path="./memory/ltm.db"    
-            )
-        )
-        entity_memory = EntityMemory(
-            storage=RAGStorage(   
-                embedder_config={
-                    "provider": "openai",
-                    "config": {
-                        "model": "text-embedding-3-small",
-                    }
-                }, 
-                type="entity",
-                path="./memory/"
-            )
-        )
+
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
-            memory=True,
-            long_term_memory=long_term_memory,
-            short_term_memory=short_term_memory,
-            entity_memory=entity_memory
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
